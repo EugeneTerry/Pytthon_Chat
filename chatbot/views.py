274290b77d8ccf3c7e3d2ee4adfc538358a9ed60
Eugene Terry <eugene.terry73@gmail.com>
2023-10-09 17:18:11 -0500

    input works finally!!!

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def chatbot(request):
-  return render (request, 'chatbot.html')
+  if request.method == 'POST':
+    message = request.POST.get('message')
+    response = 'Hi this is my response'
+    return JsonResponse({'message': message, 'response': response})
+  return render(request, 'chatbot.html')
     
